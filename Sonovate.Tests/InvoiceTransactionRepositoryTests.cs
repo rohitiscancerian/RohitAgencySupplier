53f5c124c17110a7a08699c889622f0038a63876
Rohit Kumar <rohit.kumar@arup.com>
2020-03-02 08:49:05 +0000

    Added repository tests

---
+++ Sonovate.Tests/InvoiceTransactionRepositoryTests.cs
@@ -0,0 +1,44 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Moq;
+using Raven.Client.Documents;
+using Sonovate.CodeTest.Domain;
+using Sonovate.CodeTest.Repository;
+using Sonovate.CodeTest.Service;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Sonovate.Tests
+{
+    [TestClass]
+    public class TestInvoiceTransactionRepository
+    {
+        private IInvoiceTransactionRepository _invoiceTransactionRepository;
+        [TestInitialize]
+        public void Setup()
+        {
+            _invoiceTransactionRepository = new InvoiceTransactionRepository();
+        }
+        [TestMethod]
+        public  void ShouldReturnTwoInvoiceTransactions()
+        {            
+            //Act
+            var invoiceTransactions = _invoiceTransactionRepository.GetTransactionBetweenDates(new DateTime(2019, 4, 1), new DateTime(2019, 4, 5));
+
+            //Assert
+            Assert.AreEqual(2, invoiceTransactions.Count);
+        }
+
+        [TestMethod]
+        public void ShouldNotReturnAnyInvoiceTransactions()
+        {
+
+            //Act
+            var invoiceTransactions = _invoiceTransactionRepository.GetTransactionBetweenDates(new DateTime(2020, 4, 1), new DateTime(2020, 4, 5));
+
+            //Assert
+            Assert.AreEqual(0, invoiceTransactions.Count);
+        }
+    }
+}
---
+++ Sonovate.Tests/PaymentsRepositoryTests.cs
@@ -0,0 +1,44 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Moq;
+using Raven.Client.Documents;
+using Sonovate.CodeTest.Domain;
+using Sonovate.CodeTest.Repository;
+using Sonovate.CodeTest.Service;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Sonovate.Tests
+{
+    [TestClass]
+    public class TestPaymentRepository
+    {
+        private IPaymentsRepository _paymentsRepository;
+        [TestInitialize]
+        public void Setup()
+        {
+            _paymentsRepository = new PaymentsRepository();
+        }
+
+        [TestMethod]
+        public  void ShouldReturnTwoInvoiceTransactions()
+        {
+            //Act
+            var payments = _paymentsRepository.GetPaymentsBetweenDates(new DateTime(2019, 9, 11), new DateTime(2019, 9, 29));
+
+            //Assert
+            Assert.AreEqual(4, payments.Count);
+        }
+
+        [TestMethod]
+        public void ShouldNotReturnAnyPayments()
+        {           
+            //Act
+            var payments = _paymentsRepository.GetPaymentsBetweenDates(new DateTime(2020, 4, 1), new DateTime(2020, 4, 5));
+
+            //Assert
+            Assert.AreEqual(0, payments.Count);
+        }
+    }
+}
